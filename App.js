b918503234e1f236974749cd956e1bc2f45efb00
Sumit Das <sumit.das@publicissapient.com>
2023-01-02 15:23:04 -0700

    Parcel Install

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from'react';
+import ReactDOM from 'react-dom';
+
+
 const heading = React.createElement('h1',{},'React Heading')
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(heading)
