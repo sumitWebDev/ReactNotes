7687dc14f8e177d287fc020124edeadb05bc6362
Sumit Das <sumit.das@publicissapient.com>
2023-01-02 17:25:39 -0700

    Modified Package.json and added another element to App.js

--- App.js
+++ App.js
@@ -2,6 +2,22 @@ import React from'react';
 import ReactDOM from 'react-dom';
 
 
-const heading = React.createElement('h1',{},'React Heading')
+const heading1 = React.createElement('h1',{
+    className: 'heading',
+    keys: 'h1',
+},'React Heading')
+
+const heading2 = React.createElement('h2',{
+    className: 'heading',
+    keys: 'h2',
+},'React Desc')
+
+const container = React.createElement(
+    "div",
+    {
+      id: "container"
+    },
+    [heading1, heading2]
+  );
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(heading)
+root.render(container)
