40f5ea212dc8ccc9e6796d7068285fc43ec66a46
Sumit Das <sumit.das@publicissapient.com>
2023-01-03 21:41:24 -0700

    New JSX

--- App.js
+++ App.js
@@ -1,23 +1,6 @@
 import React from'react';
 import ReactDOM from 'react-dom';
 
-
-const heading1 = React.createElement('h1',{
-    className: 'heading',
-    keys: 'h1',
-},'React Heading')
-
-const heading2 = React.createElement('h2',{
-    className: 'heading',
-    keys: 'h2',
-},'React Desc')
-
-const container = React.createElement(
-    "div",
-    {
-      id: "container"
-    },
-    [heading1, heading2]
-  );
+const heading3 = <h3>Testing JSX</h3>
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(container)
+root.render(heading3)
