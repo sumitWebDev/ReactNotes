ef4820388391f1bba6aed3a56585693fb1566905
Sumit Das <sumit.das@publicissapient.com>
2023-01-05 22:12:42 -0700

    React Composite Component

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM1 from 'react-dom/client';
 
 const HeaderComponent = () => <h1>Heading</h1>
 
-const HeaderComponent2 = function (){
-    return <div>{ HeaderComponent () } <h2>Heading 2</h2></div>
+const HeaderComponent2 =  () => {
+    return <div>{<HeaderComponent> </HeaderComponent>} <h2>Heading 2</h2></div>
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM1.createRoot(document.getElementById('root'))
 root.render(<HeaderComponent2/>)
