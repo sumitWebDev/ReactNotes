0d354403f4cdd1bd31303172d62cf7ad04ce9d5c
Sumit Das <dassumit.ece@gmail.com>
2023-01-06 23:37:24 -0700

    Food Villa App Basic Layout

--- App.js
+++ App.js
@@ -1,11 +1,78 @@
-import React from'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HeaderComponent from './components/Header'
+import './styles/index.css'
 
+const Title = () => <a href="/" > <h1>Food Villa</h1></a>
 
-const Components =  () => {
-    return <div>{<HeaderComponent/>}</div>
+const Header = function () {
+    return (
+        <div className='header'>
+            <Title />
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>Home</li>
+                    <li>Home</li>
+                    <li>Home</li>
+                </ul>
+            </div>
+        </div>)
+}
+
+const Body = () => {
+    return (
+        <h4>
+            Body
+        </h4>
+    )
+}
+const Footer = () => {
+    return (
+        <h4>
+            Footer
+        </h4>
+    )
+}
+const AppLayout = () => { //Func Component
+    return (
+        <React.Fragment>
+            <Header />
+            <Body />
+            <Footer />
+            {
+                /**
+                 * Header
+                 * - Logo
+                 * - List Items
+                 * - Cart
+                 *
+                 * Body
+                 * - search bar
+                 * - restaurants
+                 *  - Restaurant Info Cards
+                 *    - Image
+                 *    - Name
+                 *    - Description
+                 *    - Cusines
+                 *
+                 * Footer
+                 * - Links
+                 */
+            }
+        </React.Fragment>
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(Components())
+root.render(<AppLayout />)
+// import React from'react';
+// import ReactDOM from 'react-dom/client';
+// import HeaderComponent from './components/Header'
+
+
+// const Components =  () => {
+//     return <div>{<HeaderComponent/>}</div>
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(Components())
