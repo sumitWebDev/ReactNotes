8171ff257108159241ce50a8c40328f9b959b88f
Sumit Das <dassumit.ece@gmail.com>
2023-01-24 20:25:45 -0700

    Added default helper function

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,6 +1,6 @@
-export function filterData(searchText, restaurants) {
+export default function filterData(searchText, restaurants) {
     return restaurants.filter((restaurant) => {
         if (searchText)
-            return restaurant.data.name.includes(searchText)
+            return restaurant?.data?.name.includes(searchText)
     })
 }
