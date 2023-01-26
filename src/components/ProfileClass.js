69dd82c2f29f7726fb7192447c7922dec68e5a02
Sumit Das <dassumit.ece@gmail.com>
2023-01-25 22:49:08 -0700

    unmount Demo

--- src/components/ProfileClass.js
+++ src/components/ProfileClass.js
@@ -7,21 +7,36 @@ class Profile extends React.Component{
         this.state = {
             userInfo: {}
         };
+        console.log(`${this.props.name} Constructor`)
     }
     async componentDidMount (){
-        const data = await fetch ('https://api.github.com/users#sumitWebDev');
+        console.log(`${this.props.name} componentDidMount`)
+        const data = await fetch ('https://api.github.com/users/sumitWebDev');
         const json = await data.json();
+        console.log(json)
+        this.timer = setInterval(function(){
+            console.log('SetInterval');
+        },1000)
         this.setState({
             userInfo : json
         })
-        console.log(this.state)
-        console.log(json)
+        console.log(`${this.props.name} componentDidMount`)
     }
+    // componentDidUpdate = ()=>{
+
+    // }
+    componentWillUnmount = ()=>{
+        clearInterval(this.timer)
+    }
+    // async componentDidMount (){
+    //     console.log(`${this.props.name} componentDidMount`)
+    // }
     render(){
+        console.log(`${this.props.name} render`)
         return <>
-        <h1>Profile Class Component</h1>
-        {/* <img src = {this.state.userInfo[0].avatar_url} />
-        <h2>{this.state.userInfo[0].login}</h2> */}
+        <h1>{this.props.name}</h1>
+        <img src = {this.state.userInfo.avatar_url} />
+        <h2>{this.state.userInfo.login}</h2>
         </>
     }
 }
