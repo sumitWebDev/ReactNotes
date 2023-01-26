9a304d367cf2ca3c373c6648dffa194c0569e797
Sumit Das <dassumit.ece@gmail.com>
2023-01-25 23:05:42 -0700

    unmount Demo using hooks

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,17 @@
+import { useEffect } from "react"
+
 const Profile = () =>{
+    useEffect(()=>{
+        var timer = setInterval(()=>{
+            console.log('Interval')
+        },1000)
+
+        return(
+            function(){
+                   clearInterval(timer)
+            }
+        )
+    })
     return (
         <h2>Profile</h2>
     )
